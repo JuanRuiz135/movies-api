3054333cbd4d7607b3cc1b01ee30dab1727bc6b6
Juan <jdrc2908@gmail.com>
2021-02-02 09:29:01 -0500

    base implementation of error & validation handling

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandler');
+
 // body parser
 app.use(express.json());
 
+// Routes
 moviesApi(app);
+
+// Error handling, always after routes
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
     console.log(`Listening http:localhost:${config.port}`);
